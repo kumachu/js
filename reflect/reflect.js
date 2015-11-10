3d617390664b568f9f542d4902726205ebd8c21d
kuma <human12351@yahoo.co.jp>
2015-11-11 03:12:16 +0900

    recursive loop

--- reflect/reflect.js
+++ reflect/reflect.js
@@ -2,11 +2,22 @@
 	var $ = jQuery;
 	var reflectHander = {
 		start: function(target, json) {
-			for (key in json) {
-				target.find("." + key).html(json[key]);
+			reflectHtml(target, json);
+		}
+	}
+
+	function reflectHtml(target, json) {
+		for (key in json) {
+			var childTarget = target.children("." + key);
+			var value = json[key];
+			if (typeof json[key] == "string") {
+				childTarget.html(value);
+			} else {
+				reflectHtml(childTarget, value);
 			}
 		}
 	}
+
 	$.fn.reflect = function(json) {
 		var target = $(this);
 		reflectHander.start(target, json);
