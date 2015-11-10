0ff6be67a847839f017e54ceba09639c7e00502c
kuma <human12351@yahoo.co.jp>
2015-11-11 01:41:25 +0900

    reflect json value.first lv.

--- reflect/reflect.js
+++ reflect/reflect.js
@@ -1,8 +1,14 @@
-Object.prototype.reflect = function(obj) {
-	console.log(typeof obj);
-	if (typeof obj === "string") {
-		this.html(obj);
-	} else if (typeof obj === "object") {
-		console.log(obj)
+(function (jQuery) {
+	var $ = jQuery;
+	var reflectHander = {
+		start: function(target, json) {
+			for (key in json) {
+				target.find("." + key).html(json[key]);
+			}
+		}
 	}
-}
+	$.fn.reflect = function(json) {
+		var target = $(this);
+		reflectHander.start(target, json);
+	}
+})(jQuery);
