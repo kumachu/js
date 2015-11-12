8d55e92d7ab2f423eb057ab24377f992284036be
kuma <human12351@gmail.com>
2015-11-13 02:18:58 +0900

    create example page

--- reflect/reflect.js
+++ reflect/reflect.js
@@ -8,24 +8,9 @@
 			if (this.isPrimitive(response)) {
 				this.reflectValue(parentTarget, response);
 			} else if(this.isArray(response)) {
-				for (var i = response.length - 1; i >= 0; i--) {
-					if (i === response.length -1) {
-						var cloneTarget = parentTarget;
-					} else {
-						var cloneTarget = parentTarget.clone();
-						parentTarget.after(cloneTarget);
-					}
-					this.reflectHtml(parentTarget, response[i]);
-				}
+				this.reflectArray(parentTarget, response);
 			} else {
 				this.reflectObject(parentTarget, response);
-			}
-		},
-		reflectObject: function(parentTarget, response) {
-			for (key in response) {
-				var target = parentTarget.children("." + key);
-				var value = response[key];
-				this.reflectHtml(target, value);
 			}
 		},
 		reflectValue: function (target, value) {
@@ -33,6 +18,24 @@
 				target.val(value);
 			} else {
 				target.html(value);
+			}
+		},
+		reflectArray: function(target, values) {
+			for (var i = values.length - 1; i >= 0; i--) {
+				if (i === values.length -1) {
+					var cloneTarget = target;
+				} else {
+					var cloneTarget = target.clone();
+					target.after(cloneTarget);
+				}
+				this.reflectHtml(target, values[i]);
+			}
+		},
+		reflectObject: function(parentTarget, response) {
+			for (key in response) {
+				var target = parentTarget.children("." + key);
+				var value = response[key];
+				this.reflectHtml(target, value);
 			}
 		},
 		isPrimitive: function(value) {
